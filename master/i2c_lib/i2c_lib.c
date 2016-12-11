9c82ecbdf2f866eefedf3435b344cfb6c3020f4f
Lakshay Somani <Lakshay Somani>
2016-12-11 15:15:22 +0530

    Header file and c library file is written

--- master/i2c_lib/i2c_lib.c
+++ master/i2c_lib/i2c_lib.c
@@ -1,4 +1,5 @@
 /*
+
  * i2c_lib.c
  *
  * Created: 11/14/2016 3:43:26 PM
@@ -26,7 +27,7 @@ void i2c_Init(void)
 /************************************************************************/
 void i2c_Wait(void);
 {
-	while ((I2C_CR & 0x81) || (I2C_ST & )); //Transmit is in progress
+	while ((I2C_CR & 0x81) || (I2C_ST & 0x04)); //Transmit is in progress
 }
 
 /************************************************************************/
@@ -35,6 +36,11 @@ void i2c_Wait(void);
 void i2c_Start(void)
 {
 	i2c_Wait();
+	DDRB=0x03;
+	if (tm=1)
+	{
+		PORTB=0x02;
+	}
 	I2C_CR=0x20;  //It will start the i2c
 }
 
@@ -61,7 +67,15 @@ void i2c_Stop(void)
 /************************************************************************/
 void i2c_Write(unsigned char data)
 {
-	i2c_Wait();
+	DDRB=0x00;
+	for (unsigned char c=0;c<8;c++)
+	{
+		data =I2C_DR;
+		PORTB=(data<<1)&(0x03);
+		data=data>>1;
+	}
+	read_ack();
+	I2C_ST=0x09;
 }
 
 /************************************************************************/
@@ -70,17 +84,87 @@ void i2c_Write(unsigned char data)
 /************************************************************************/
 void i2c_Address(unsigned char address, unsigned char mode)
 {
-	
+	i2c_Start();
+	add=(add<<1)+mode;
+	DDRB=0x00;
+	for (unsigned char c=0;c<8;c++)
+	{
+		PORTB=(add<<1)&(0x03);
+		add=add>>1;
+	}
+	read_ack();
+	I2C_ST=0x10;
 }
 
 /************************************************************************/
-/*    i2c_Read - Reads a byte from Slave device                                                                  */
+/*    i2c_Read - Slave is reading the address                                                                  */
 /************************************************************************/
-unsigned char i2c_Read(unsigned char ack)
+void I2C_SLAVE_read_add(unsigned int PINB){
+	add=0;   //assigning initial value to address
+	for(int i=128;i>=1;i=i/2){
+		PINB=PINB*i;		//decimal value of bit
+		if(i==1){			//store 8th bit for read or write operation
+			c=PINB;		//assign value to c
+		}
+		else{				//calculation of address
+			add=add+PINB;	//rest of bits are assumed as address bits
+		}
+	}
+	address(add);					//calling of address function
+}
+/************************************************************************/
+/*    i2c_Read - Slave is reading the address                                                                  */
+/************************************************************************/
+void I2C_SLAVE_read_data(unsigned int PINB){		//slave_read_data function definition
+	for(int i=128;i>=1;i=i/2){
+		PINB=PINB*i;					//value of PINB signal
+		temp=temp+PINB;					//storing of calculated data in temp
+	}
+	LCD_Display(temp);					//display data at LCD
+	temp=0x00;							//reset the temp
+}
+
+/************************************************************************/
+/*    i2c_ack - Sends a acknowledgment whenever data is received                                                                  */
+/************************************************************************/
+void i2c_read_ack()
 {
 	
+	DDRB=0x01;
+	if (PINB&0x01)
+	{
+		I2C_ST=0x08;
+	}
+}
+void I2C_write_ack(){		//function definition
+	PORTB=0x00;							//initialize port B
+	_delay_ms(10);						//delay for 10ms
+	PORTB=0x02;
+	I2C_ST=0x10;						//second pin of portB is high
+}
+/************************************************************************/
+/*    i2c_timer - timer or the i2c                                                                  */
+/************************************************************************/
+ISR(TIMER0_COMPA_vect)
+{
+	tm=~tm;
 }
 
+void timer_init()
+{
+	TCCR0A=0xc3;
+	TCCR0B=0x01;
+	TIMSK0=0x02;
+	TCNT0=0xff;
+}
+int timer(void)
+{
+	sei();
+	DDRB=tm;
+	timer_init();
+	OCR0A=1;
+	_delay_ms(5);
+}	
 /************************************************************************/
 /*                                                                      */
 /************************************************************************/
--- master/i2c_lib/i2c_lib.h
+++ master/i2c_lib/i2c_lib.h
@@ -14,17 +14,21 @@
 #define I2C_ST uint8_t
 #define I2C_ADD uint8_t
 #define I2C_DR uint8_t
+#define tm uint8_t
 
 #define I2C_WRITE 0
 #define I2C_READ 1
 
 void i2c_Init(void);
+void i2c_read_ack(void);
+void I2C_write_ack(void);
 void i2c_Wait(void);
 void i2c_Start(void);
 void i2c_Restart(void);
 void i2c_Stop(void);
 void i2c_Write(unsigned char data);
 void i2c_Address(unsigned char address, unsigned char mode);
-unsigned char i2c_Read(unsigned char ack);
+void I2C_SLAVE_read_add(unsigned int SDA);
+void I2C_SLAVE_read_data(unsigned int SDA);
 
 #endif /* I2C_LIB_H_ */
